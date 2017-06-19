4ed30f4d121786653508e6bcfe90e68907c7bb74
apexa11 <apexachoxi7@gmail.com>
2017-06-19 15:58:35 -0400

    update

--- Vagrantfile
+++ Vagrantfile
@@ -3,8 +3,8 @@
 
 Vagrant.configure("2") do |config|
   config.vm.box = "bento/ubuntu-16.04-i386"
-  config.vm.network "forwarded_port", guest: 8000, host: 8082, host_ip: "127.0.0.1"
-  config.vm.network "forwarded_port", guest: 8080, host: 8092, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 8080, host: 2210, host_ip: "127.0.0.1"
+  config.vm.network "forwarded_port", guest: 8085, host: 8085, host_ip: "127.0.0.1"
   config.vm.network "forwarded_port", guest: 5000, host: 2019, host_ip: "127.0.0.1"
   # Work around disconnected virtual network cable.
   config.vm.provider "virtualbox" do |vb|
---
+++ catalog/webserver.py
@@ -0,0 +1,34 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+
+class WebServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith("/hello"):
+            self.send_response(200)
+            self.send_header('Content-type','text/html')
+            self.end_headers()
+            output=""
+            output+="<html><body>Hello!</body></html>"
+            self.wfile.write(output)
+            print output
+            return
+
+        else:
+            self.send_error(404, "File Not Found %s" % self.path)
+
+
+
+def main():
+    try:
+        port=2210
+        server = HTTPServer(('',port),WebServerHandler)
+        print"web server running on %s" % port
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        print "^C enter stopping webserver ..."
+        server.socket.close()
+
+
+if __name__ == '__main__':
+    main()
