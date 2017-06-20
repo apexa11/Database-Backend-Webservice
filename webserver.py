01eac9e74717a98e78404d68e586df645dcae360
apexa11 <apexachoxi7@gmail.com>
2017-06-19 22:13:08 -0400

    update

--- webserver.py
+++ webserver.py
@@ -22,19 +22,30 @@ class WebServerHandler(BaseHTTPRequestHandler):
             self.end_headers()
             output=""
             output+="<html><body>"
+            output+="<a href = '/restaurants/new'> Make New Restaurant Here </a>"
+            output+="<br/>"
             for restaurant in restaurants:
                 output+= restaurant.name
                 output+="<br/>"
+                output+="<a href = '/restaurants/%s/edit' > Edit </a>"
+                output+="<br/>"
+                output+="<a href = '#' > Delete </a>"
+                output+="<br/>"
+
             output+="</body></html>"
             self.wfile.write(output)
             return
 
-        if self.path.endswith("/hola"):
+        if self.path.endswith("/restaurants/new"):
+
             self.send_response(200)
             self.send_header('Content-type','text/html')
             self.end_headers()
             output=""
-            output+="<html><body>Hola!<a href = '/hello'> Back to hello </a> </body></html>"
+            output+="<html><body>"
+            output+="New Restaurant"
+            output += '''<form method='POST' enctype='multipart/form-data' action='/restaurants/new'><input name="newRestaurantName" type="text" ><input type="submit" value="Create"> </form>'''
+            output+=" </body></html>"
             self.wfile.write(output)
             print output
             return
@@ -44,26 +55,39 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-            ctype, pdict = cgi.parse_header(
-                self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                messagecontent = fields.get('message')
-            output = ""
-            output += "<html><body>"
-            output += " <h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % messagecontent[0]
-            output += '''<form method='POST' enctype='multipart/form-data' action='/hello'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
-            output += "</body></html>"
-            self.wfile.write(output)
-            print output
+            if self.path.endswith("/restaurants/new"):
+
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+                messagecontent = fields.get('newRestaurantName')
+                newRestaurant = Restaurant(name = messagecontent[0])
+                session.add(newRestaurant)
+                session.commit()
+
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+            #self.send_response(301)
+            #self.send_header('Content-type', 'text/html')
+            #self.end_headers()
+            #ctype, pdict = cgi.parse_header(
+             #   self.headers.getheader('content-type'))
+            #if ctype == 'multipart/form-data':
+             #   fields = cgi.parse_multipart(self.rfile, pdict)
+              #  messagecontent = fields.get('message')
+            #output = ""
+            #output += "<html><body>"
+            #output += " <h2> Okay, how about this: </h2>"
+            #output += "<h1> %s </h1>" % messagecontent[0]
+            #output += '''<form method='POST' enctype='multipart/form-data' action='//restaurants/new'><h2>What would you like me to say?</h2><input name="message" type="text" ><input type="submit" value="Submit"> </form>'''
+            #output += "</body></html>"
+            #self.wfile.write(output)
+            #print output
         except:
             pass
-
-
 
 def main():
     try:
