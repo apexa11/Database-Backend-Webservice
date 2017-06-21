780993c2d2bce555ca73899ca6c8c6dc1196470c
apexa11 <apexachoxi7@gmail.com>
2017-06-21 15:09:35 -0400

    update

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template ,request , url_for,redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
