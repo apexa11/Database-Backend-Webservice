7509e31128f2459b3ea838ef33c1a1aa0afddf00
apexa11 <apexachoxi7@gmail.com>
2017-06-21 14:54:26 -0400

    update

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask ,render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -11,16 +11,32 @@ app = Flask(__name__)
 
 #import database
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
+@app.route('/restaurant/<int:restaurant_id>/')
+def restaurant():
     restaurant = session.query(Restaurant).first()
     menu = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output=''
+        return render_template('menu.html',restaurant=restaurant,menu=menu)
 
-    for items in menu:
-        output+=items.name
-        output+= "<br>/"
-        return output
+# Task 1: Create route for newMenuItem function here
+
+@app.route('restaurant/<int:restaurant_id>/new/', methods = ['GET','POST'])
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+
+@app.route('restaurant/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+
+@app.route('restaurant/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
+
 
 if __name__ == '__main__':
     app.debug = True
